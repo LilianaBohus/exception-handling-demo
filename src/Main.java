e454541346db42cbd6b7119e60737f84f3fda8a8
Liliana Bohus <boli96@gmail.com>
2023-02-13 14:53:24 +0100

    extract method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Szervezzük ki a beolvasást egy metódusba!
-        // (Aminek a paramétere a fájl elérési útvonala lesz)
+        readFromFile("nem_letezo_fajl.txt");
+
+        /*
+        Itt még hibás a kód, megint le kell kezelnünk a kivételeket.
+        (Soha ne pusholjunk nem forduló kódot!)
+         */
+    }
+
+    private static void readFromFile(String path) throws FileNotFoundException {
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
     }
 }
