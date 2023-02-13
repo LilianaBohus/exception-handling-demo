fe6c5cc6ca67e67f3a5be335b0f14a1fea4742f1
Liliana Bohus <boli96@gmail.com>
2023-02-13 14:46:27 +0100

    throw exception

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,10 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // Próbáljunk meg beolvasni egy nem létező fájlból!
         File file = new File("nem_letezo_fajl.txt");
-        try {
-            Scanner scanner = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            System.out.println("Nem található ilyen fájl!");
-        }
+        Scanner scanner = new Scanner(file);
 
     }
 }
