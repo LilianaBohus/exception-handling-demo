1443158243489bb71498aac46213d28ea69fa556
Liliana Bohus <boli96@gmail.com>
2023-02-13 14:43:39 +0100

    try-catch handling

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // Próbáljunk meg beolvasni egy nem létező fájlból!
         File file = new File("nem_letezo_fajl.txt");
-        Scanner scanner = new Scanner(file);
+        try {
+            Scanner scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("Nem található ilyen fájl!");
+        }
 
-        /*
-         Ez a kód még hibás: nincs lekezelve az előfordulható checked exception.
-         (Soha ne pusholj nem forduló kódot!)
-         */
     }
 }
