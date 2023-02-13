17dc00770a4691ee212a8ab1eaad54bb81101c86
Liliana Bohus <boli96@gmail.com>
2023-02-13 14:38:42 +0100

    add file reading

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.io.File;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // Próbáljunk meg beolvasni egy nem létező fájlból!
+        File file = new File("nem_letezo_fajl.txt");
+        Scanner scanner = new Scanner(file);
+
+        /*
+         Ez a kód még hibás: nincs lekezelve az előfordulható checked exception.
+         (Soha ne pusholj nem forduló kódot!)
+         */
     }
 }
