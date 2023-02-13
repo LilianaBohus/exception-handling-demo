453c11e04ae0c52609328fa16e5cedeb24e9ad10
Liliana Bohus <boli96@gmail.com>
2023-02-13 14:01:57 +0100

    add implementation without exception handling

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Kérjünk be a felhasználótól két számot és osszuk el őket!
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Add meg az osztandó számot!");
+        int osztando = Integer.parseInt(scanner.nextLine());
+        System.out.println("Add meg az osztót!");
+        int oszto = Integer.parseInt(scanner.nextLine());
+
+        double hanyados = (double) osztando / oszto;
+        System.out.println("A két szám hányadosa: " + hanyados);
     }
 }
