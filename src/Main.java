37a5193e97f38c8e6400e759a96f38982f7360f2
Liliana Bohus <boli96@gmail.com>
2023-02-13 14:21:25 +0100

    add exception handling

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,18 @@ public class Main {
         System.out.println("Add meg az osztót!");
         int oszto = Integer.parseInt(scanner.nextLine());
 
-        // Próbáljunk nullával osztani!
-        double tortHanyados = (double) osztando / oszto;
-        System.out.println("A két szám tört hányadosa: " + tortHanyados);
-        int egeszHanyados = osztando / oszto;
-        System.out.println("A két szám egész hányadosa: " + egeszHanyados);
+        // Kezeljük le az előfordul(hat)ó hibákat!
+        try {
+            int egeszHanyados = osztando / oszto;
+            System.out.println("A két szám egész hányadosa: " + egeszHanyados);
+        } catch (ArithmeticException e) {
+            System.out.println("Hiba történt: " + e.getMessage());
+        }
+
+        /* Tanulság:
+        Java-ban a RuntimeException-ök unchecked exceptionnek számítanak,
+        tehát nem kötelez minket a compiler arra, hogy lekezeljük őket,
+        ettől függetlenül mi megtehetjük.
+         */
     }
 }
